d260237f9d5798446124ec8eaa366fc08c607e93
saman-dadmand <saman.dadmand@gmail.com>
2021-04-06 11:02:43 +0430

    Update get_mark_price.py

--- example/market/get_mark_price.py
+++ example/market/get_mark_price.py
@@ -2,8 +2,8 @@ from binance_f import RequestClient
 from binance_f.base.printobject import *
 from binance_f.constant.test import *
 
-proxyDict = dict(http='http://eniac-service.ir:53128',
-             https='http://eniac-service.ir:53128')
+proxyDict = dict(http='http://ip:port',
+             https='http://ip:port')
 request_client = RequestClient(api_key=g_api_key, secret_key=g_secret_key, proxy=proxyDict)
 
 result = request_client.get_mark_price(symbol="BTCUSDT")
